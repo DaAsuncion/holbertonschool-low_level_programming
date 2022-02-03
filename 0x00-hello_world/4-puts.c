1110315d9b33afa39854cf9c0b0f4f7a806a72f6
DaAsuncion <daniserafim_11@hotmail.com>
2022-02-03 11:42:12 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
-#include <string.h>
-int main()
+/**
+ *main - entry
+ *Return: 0
+ */
+int main(void)
 {
- char string[40];
- strcpy(str, "Programming is like building a multilingual puzzle");
- puts(string);
- return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
