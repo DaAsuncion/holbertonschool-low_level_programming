89b8c3779b315798c860e6392ca5e91c996c94dc
DaAsuncion <daniserafim_11@hotmail.com>
2022-02-03 11:30:47 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#!/bin/bash
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+ char string[];
+ strcpy(str, "Programming is like building a multilingual puzzle");
+ puts(string);
+ return 0;
+}
