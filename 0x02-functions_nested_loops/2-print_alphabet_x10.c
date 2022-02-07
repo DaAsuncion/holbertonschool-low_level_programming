c7713f29487561461a34cb805a74900020bf418a
DaAsuncion <daniserafim_11@hotmail.com>
2022-02-07 10:20:56 -0800

    modificate

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,9 +4,9 @@
  **print_alphabet_x10 - Write a function that prints 10 times the alphabet
  **Return: 0
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
-	char (ch[10]);
+	char ch[10];
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 {
