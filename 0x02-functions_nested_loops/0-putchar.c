2f43883e230f0f4467b223735fcf4bfb64669ab4
DaAsuncion <daniserafim_11@hotmail.com>
2022-02-07 08:03:31 -0800

    modify error 3

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
+#include "main.h"
 /**
  **main - Write a program that prints _putchar, followed by a new line
  **Return: 0
  */
-int main(void)
+	int main(void)
 {
-	char _putchar = ('_') ('p') ('u') ('t') ('c') ('h') ('a') ('r');
+	putchar ('_');
+	putchar ('p');
+	putchar ('u');
+	putchar ('t');
+	putchar ('c');
+	putchar ('h');
+	putchar ('a');
+	putchar ('r');
 
-	putchar(_putchar);
 	return (0);
 }
 
