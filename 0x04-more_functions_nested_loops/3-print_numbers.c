964047219714e121f6b8e336e98baf66af4ac719
DaAsuncion <daniserafim_11@hotmail.com>
2022-02-09 13:51:26 -0800

    prints the numbers, from 0 to 9 prove 8

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_numbers - prints the numbers, from 0 to 9 
+ * print_numbers - prints the numbers, from 0 to 9
  */
 void print_numbers(void)
 {
@@ -9,7 +9,6 @@ void print_numbers(void)
 	for (num = 0; num <= 9; num++)
 {
 	_putchar(num + '0');
-}	
-
+}
 	_putchar('\n');
 }
