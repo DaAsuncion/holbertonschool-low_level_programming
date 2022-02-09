83cd96f9f37c100c6c8d8498e86b9d132e9d1743
DaAsuncion <daniserafim_11@hotmail.com>
2022-02-09 14:50:46 -0800

    Do not print 2 and 4 prove 3

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,10 +7,12 @@ void print_most_numbers(void)
 	int num;
 
 	for (num = 0; num <= 9; num++)
-{
+	{
 	if (num == 2 || num == 4)
-	num++;
-}
+	{
+		num++;
+	}
 	_putchar(num + '0');
 	_putchar('\n');
+	}
 }
