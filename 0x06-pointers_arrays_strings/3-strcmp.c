505d3169afca7f69cb42d0ba6994ca0cf2a91911
DaAsuncion <daniserafim_11@hotmail.com>
2022-02-15 13:27:47 -0800

    Write a function that compares two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcmp - compares two strings
+ * @s1: string
+ * @s2: string
+ * Return: 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int a = 0;
+	int b = 0;
+
+	for (s1[a] != '\0' && b == 0);
+	{
+		b = s1[a] - s2[a];
+		a++;
+	}
+	return (b);
+}
