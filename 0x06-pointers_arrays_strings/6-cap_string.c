210747abe3f4582b602f847c1cee9fc0ae4f1658
DaAsuncion <daniserafim_11@hotmail.com>
2022-02-16 09:46:51 -0800

    fix error 4

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,22 +2,23 @@
 #include <stdio.h>
 /**
  * cap_string - capitalizes all words of a string
- * @p: name cond.
- * Return: p
+ * @n: name cond.
+ * Return: n
 */
-char *cap_string(char *p)
+char *cap_string(char *n)
 {
-	int c = 0;
-	
-	for (; p[c]; c++)
+	int i = 0;
+
+	if (n[i] > 96 && n[i] < 123)
+		n[i] = n[i] - 32;
+	for (; n[i]; i++)
 	{
-	if (p[c] == 44 || p[c] == 59 || p[c] == 46 || p[c] == 33 || p[c] == 63 ||
-			p[c] == 34 || p[c] == 40 || p[c] == 41 || p[c] == 123 ||
-			p[c] == 125 || p[c] == 9 || p[c] == 10 || p[c] == 32 || p[c] == 0)
-	{
-	if (p[c + 1] > 96 && p[c + 1] < 123)
-		p[c + 1] = p[c + 1] - 32;
+		if (n[i] == 9 || n[i] == 10 || n[i] == 32 || n[i] == 33 ||
+				n[i] == 34 || n[i] == 40 || n[i] == 41 || n[i] == 44 ||
+				n[i] == 46 || n[i] == 59 || n[i] == 63 || n[i] == 123 ||
+				n[i] == 125)
+			if (n[i + 1] > 96 && n[i + 1] < 123)
+				n[i + 1] = n[i + 1] - 32;
 	}
-	}
-	return (p);
+	return (n);
 }
