e1d1a33be5a1d19c63fbd81e3113d48015c1e965
DaAsuncion <daniserafim_11@hotmail.com>
2022-02-16 06:06:10 -0800

    fix error

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,21 +1,21 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * cap_string - capitalizes all words of a string
- *
- * Return: Always 0.
- */
+ * @p: name cond.
+ * Return: p
+*/
 char *cap_string(char *p)
 {
 	int c = 0;
 
-
 	for (; p[c]; c++)
 	{
-	if (p[c] == 44 || p[c] == 59 || p[c] == 46 || p[c] == 33 || p[c] == 63 || p[c] == 34 || p[c] == 40 || p[c] == 41 || p[c] == 123 || p[c] == 125 || p[c] == 9 || p[c] == 10 || p[c] == 32)
+	if (p[c] == 44 || p[c] == 59 || p[c] == 46 || p[c] == 33 || p[c] == 63 ||
+			p[c] == 34 || p[c] == 40 || p[c] == 41 || p[c] == 123 ||
+			p[c] == 125 || p[c] == 9 || p[c] == 10 || p[c] == 32)
 	{
-	if (p[c + 1] > 96 && p[c + 1] < 123) 
+	if (p[c + 1] > 96 && p[c + 1] < 123)
 		p[c + 1] = p[c + 1] - 32;
 	}
 	}
