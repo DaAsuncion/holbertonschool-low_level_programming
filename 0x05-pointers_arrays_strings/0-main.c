d0b1f6406048d2c20a6b1dd320841db31600df69
DaAsuncion <97999637+DaAsuncion@users.noreply.github.com>
2022-02-18 00:10:27 -0300

    Update 0-main.c

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 
 /**
- *  * main - check the code 
- *   *
- *    * Return: Always 0.
- *     */
+ * void-  takes a pointer to an int as parameter and updates 
+ * the value it points to to 98 
+ * Return: 0
+*/
 int main(void)
 {
 	    int n;
 
-	        n = 402;
-		    printf("n=%d\n", n);
-		        reset_to_98(&n);
-			    printf("n=%d\n", n);
-			        return (0);
+	n = 402;
+	printf("n=%d\n", n);
+	reset_to_98(&n);
+	printf("n=%d\n", n);
+	return (0);
 }
