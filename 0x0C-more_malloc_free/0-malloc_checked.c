1ed98691637681c09b4ce17a9ba4060180abcff5
DaAsuncion <daniserafim_11@hotmail.com>
2022-03-03 10:57:37 -0800

    0. Trust no one

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,3 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 /**
@@ -13,7 +11,6 @@ void *malloc_checked(unsigned int b)
 
 	if (p == NULL)
 		exit(98);
-
-		return (p);
+	return (p);
 }
 
