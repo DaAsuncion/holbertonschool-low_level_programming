9ab8764ea117fd99f1978be8f387ce3e64cf4781
DaAsuncion <daniserafim_11@hotmail.com>
2022-03-10 06:55:43 -0800

    fix error 5

--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -1,4 +1,4 @@
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-void dog_t *new_dog(char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
