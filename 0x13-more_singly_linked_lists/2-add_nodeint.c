5455b6813c5dcc4aac2eb308d4c74f4950130aaf
DaAsuncion <daniserafim_11@hotmail.com>
2022-03-22 07:57:37 -0700

    repare betty

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -5,12 +5,15 @@
 /**
  * add_nodeint - adds a new node at the beginning
  * of a listint_t list.
+ * @head: * to adress
+ * @n: intfor new node
  * Return: address of the new element ||
  * NULL if it failed
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
+
 	new = malloc(sizeof(listint_t));
 
 	if (new == NULL)
